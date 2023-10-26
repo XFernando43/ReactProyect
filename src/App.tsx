97f86a3dfe2683b4733c453330bafa51387dcf81
XFernando43 <U20171E997@upc.edu.pe>
2023-10-26 01:31:34 -0500

    add proyect

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App =()=>{
+  const url = import.meta.env.BASE_URL;
+  return (
+    <>
+    MAIN
+    </>
+  );
+}
+
+export default App
